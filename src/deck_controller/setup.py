880f20997ec40af3e99ffa0c8240ab3bb9f467b0
Josh Cooper <hi@joshc.uk>
2025-03-26 00:14:28 +0000

    feat: add launch file, but dont use it yet

---
+++ src/deck_controller/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'deck_controller'
+pkg = package_name
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{pkg}']),
+        (f'share/{pkg}', ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Josh Cooper',
+    maintainer_email='hi@joshc.uk',
+    description='Scripts and launch files for controlling ros with a steam deck',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
